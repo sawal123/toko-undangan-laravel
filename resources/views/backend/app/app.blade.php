a3da6f7ef719b2dd75e6433e3bda4af2ee11d402
sawal123 <sawalinto@gmail.com>
2024-06-13 16:33:05 +0700

    feat: meta

--- resources/views/backend/app/app.blade.php
+++ resources/views/backend/app/app.blade.php
@@ -7,7 +7,7 @@
     <meta charset="utf-8" />
     <title>Metrica - Admin & Dashboard Template</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+    <meta content="Cetak Undangan atau Undangan digital dengan garansi 100%" name="description" />
     <meta content="" name="author" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     @vite('resources/css/app.css')
