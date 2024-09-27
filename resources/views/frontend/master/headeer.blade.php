1edcd522602dd162f46581856318996c881802d0
sawal123 <sawalinto@gmail.com>
2024-09-27 13:51:08 +0700

    feat: Revisi Header

--- resources/views/frontend/master/headeer.blade.php
+++ resources/views/frontend/master/headeer.blade.php
@@ -77,13 +77,13 @@
                     </div>
                     <div class="py-6">
                         @if (Auth::user())
-                            <a href="{{ url('login') }}"
-                                class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Log
-                                in</a>
-                        @else
                             <a href="{{ url('logout') }}"
                                 class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Log
                                 out</a>
+                        @else
+                            <a href="{{ url('login') }}"
+                                class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Log
+                                in</a>
                         @endif
                     </div>
                 </div>
